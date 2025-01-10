fa64606fdfa7c6c063c98f5eb8f9998c2712653e
noash <shvets@post.bgu.ac.il>
2025-01-10 12:21:48 +0200

    Extracted protocol functions to different file.

---
+++ hackathon/protocol.py
@@ -0,0 +1,38 @@
+import struct
+
+MAGIC_COOKIE = 0xabcddcba.to_bytes(4, byteorder="big")
+HEADER_FORMAT = "4sB"  # Protocol (4 bytes) + Message Type (1 byte)
+OFFER_MESSAGE_TYPE = 0x2
+REQUEST_MESSAGE_TYPE = 0x3
+PAYLOAD_MESSAGE_TYPE = 0x4
+MESSAGES_FORMATS = {
+    OFFER_MESSAGE_TYPE: ">HH",
+    REQUEST_MESSAGE_TYPE: ">Q",
+    PAYLOAD_MESSAGE_TYPE: ">QQ",
+}
+
+
+def parse_header(data: bytes) -> int:
+    magic_cookie, message_type = struct.unpack(HEADER_FORMAT, data)  # TODO: Add error handling
+    if magic_cookie != MAGIC_COOKIE:
+        raise ValueError(f"Wrong protocol type, got {magic_cookie} expected {MAGIC_COOKIE}")
+    return message_type
+
+
+def parse_request(data: bytes) -> int:
+    return struct.unpack(MESSAGES_FORMATS[REQUEST_MESSAGE_TYPE], data)[0]
+
+
+def build_offer_message(tcp_port, udp_port):
+    message = struct.pack(MESSAGES_FORMATS[OFFER_MESSAGE_TYPE], udp_port, tcp_port)
+    return build_header(OFFER_MESSAGE_TYPE) + message
+
+
+def build_payload(total_segments: int, segment_number: int, payload_data: bytes) -> bytes:
+    message = struct.pack(MESSAGES_FORMATS[PAYLOAD_MESSAGE_TYPE], total_segments, segment_number) + payload_data
+    return build_header(PAYLOAD_MESSAGE_TYPE) + message
+
+
+def build_header(message_type: int) -> bytes:
+    return struct.pack(HEADER_FORMAT, MAGIC_COOKIE, message_type)
+
--- hackathon/server.py
+++ hackathon/server.py
@@ -4,21 +4,13 @@ import threading
 import time
 from typing import Tuple
 
+from hackathon.protocol import build_payload, build_offer_message, REQUEST_MESSAGE_TYPE, parse_header, HEADER_FORMAT, \
+    parse_request, MESSAGES_FORMATS
+
 UDP_PAYLOAD_SIZE = 512
 BROADCAST_INTERVAL = 1
 
 BROADCAST_ADDR = ("255.255.255.255", 12345)
-
-MAGIC_COOKIE = 0xabcddcba.to_bytes(4, byteorder="big")
-HEADER_FORMAT = "4sB"  # Protocol (4 bytes) + Message Type (1 byte)
-OFFER_MESSAGE_TYPE = 0x2
-REQUEST_MESSAGE_TYPE = 0x3
-PAYLOAD_MESSAGE_TYPE = 0x4
-MESSAGES_FORMATS = {
-    OFFER_MESSAGE_TYPE: ">HH",
-    REQUEST_MESSAGE_TYPE: ">Q",
-    PAYLOAD_MESSAGE_TYPE: ">QQ",
-}
 
 
 def main():
@@ -60,31 +52,6 @@ def start_broadcasting_offer(udp_port: int, tcp_port: int):
     while True:
         send_broadcast(offer_message)
         time.sleep(BROADCAST_INTERVAL)
-
-
-def parse_header(data: bytes) -> int:
-    magic_cookie, message_type = struct.unpack(HEADER_FORMAT, data)  # TODO: Add error handling
-    if magic_cookie != MAGIC_COOKIE:
-        raise ValueError(f"Wrong protocol type, got {magic_cookie} expected {MAGIC_COOKIE}")
-    return message_type
-
-
-def parse_request(data: bytes) -> int:
-    return struct.unpack(MESSAGES_FORMATS[REQUEST_MESSAGE_TYPE], data)[0]
-
-
-def build_offer_message(tcp_port, udp_port):
-    message = struct.pack(MESSAGES_FORMATS[OFFER_MESSAGE_TYPE], udp_port, tcp_port)
-    return build_header(OFFER_MESSAGE_TYPE) + message
-
-
-def build_payload(total_segments: int, segment_number: int, payload_data: bytes) -> bytes:
-    message = struct.pack(MESSAGES_FORMATS[PAYLOAD_MESSAGE_TYPE], total_segments, segment_number) + payload_data
-    return build_header(PAYLOAD_MESSAGE_TYPE) + message
-
-
-def build_header(message_type: int) -> bytes:
-    return struct.pack(HEADER_FORMAT, MAGIC_COOKIE, message_type)
 
 
 def send_broadcast(message):
